e1ab6ec940642bcea07d28984e0849f1f295a811
Andrew Fitzgerald <fitzgeralda2010@gmail.com>
2015-08-05 11:01:08 -0400

    generated multimodal samples matrix

--- pymaxflow/abswap_multimodal.py
+++ pymaxflow/abswap_multimodal.py
@@ -5,10 +5,10 @@ import sys
 import time
 from sklearn.linear_model import LogisticRegression
 from itertools import combinations
-import matplotlib.pyplot as plt
 import math
 from utils import non_graph_weight_addition,non_graph_weight_addition_with_count, calculate_boundary_stats, calculate_boundary_stats_lgr
 import json
+import dicom
 
 def neighbor_cost_boykov(p1, p2, alpha=100):
     pdiff = np.subtract(p1,p2)
@@ -31,49 +31,46 @@ if len(sys.argv) < 2:
     print "usage: python abswap.py json_config"
     exit()
 
-bound_dict = {'0':'boykov',
-              '1':'gaussian',
-              '2':'lgr'}
-
-reg_dict = {'0':'gaussian',
-            '1':'lgr'}
-
 num_objs = 2
 
 f = open(sys.argv[1],'r')
 task = json.load(f)
 f.close()
 
-training_images = [p for p in task['training_images']]
+training_image = task['training_images']
 modalities = [m for m in task['modalities']]
 training_image_index = task['training_image_index']
 case = task['case']
 prefix = task['prefix']
 outdir =  case + '/' + task['outdir']
 dicomdir = task['dicomdir']
+regional_method = task['regional_weights']
+boundary_method = task['boundary_weights']
 
 dirs = [case +"/"+dicomdir+"/" + m for m in modalities]
+num_modalities = len(dirs)
 mmdirs = []
 
 for d in dirs:
     fil = d+'/IMG00'+ str(training_image_index)+'.dcm'
     mmdirs.append(fil)
 
+brush_img = imread(str(training_image))
 
-'''
-im_shape = actual_img.shape
+im_shape = brush_img.shape
 width = im_shape[0]
 height = im_shape[1]
+im_size = width*height
+brush_img = brush_img.reshape((im_size,3))
 
-actual_img = actual_img.reshape((width*height,1))
-brush_img = brush_img.reshape((width*height,3))
-
-indices = np.arange(actual_img.size).reshape(im_shape).astype(np.int32)
+indices = np.arange(im_size).reshape((width,height)).astype(np.int32)
 
 labels = np.random.randint(0,num_objs,(width*height)).astype(np.int32)
 regional_weights = np.zeros((width*height,num_objs))
 
-graph_size = width*height
+image_mat = np.zeros((im_size,num_modalities))
+for i,img in enumerate(mmdirs):
+    image_mat[:,i] = dicom.read_file(str(img)).pixel_array.reshape((im_size))
 
 ####################################################
 #                                                  #
@@ -81,10 +78,8 @@ graph_size = width*height
 #                                                  #
 ####################################################
 
-obj0samples = []
-obj1samples = []
-obj2samples = []
-obj3samples = []
+samples0 = []
+samples1 = []
 
 #masks for samples and intial labeling
 red_band = brush_img[:,0]
@@ -98,21 +93,23 @@ yellow_mask_1 = np.logical_and(red_band == 255,green_band == 255)
 yellow_mask = np.logical_and(yellow_mask_1,blue_band==0)
 
 
-labels[yellow_mask] = 0
+labels[blue_mask] = 0
 labels[red_mask] = 1
-labels[blue_mask] = 2
-labels[green_mask] = 3
 
-#order matters so that initial labeling and probabilities are correct
-obj0samples = actual_img[yellow_mask]
-obj1samples = actual_img[red_mask]
-obj2samples = actual_img[blue_mask]
-obj3samples = actual_img[green_mask]
+for img in mmdirs:
+    current_im = dicom.read_file(str(img)).pixel_array.reshape((im_size))
+    samples0.append(current_im[blue_mask])
+    samples1.append(current_im[red_mask])
 
-brush_strokes = [obj0samples,obj1samples,obj2samples,obj3samples]
+#setup array to pass to fit
+obj0samples = np.zeros((samples0[0].size,num_modalities))
+obj1samples = np.zeros((samples1[0].size,num_modalities))
 
-means, stds = calculate_boundary_stats(brush_strokes,100)
-combs = int(comb(num_objs,2))
+for i in range(num_modalities):
+    obj0samples[:,i] = samples0[i]
+    obj1samples[:,i] = samples1[i]
+
+print type(samples0[0])   
 
 ##############################################
 #                                            #
@@ -120,7 +117,7 @@ combs = int(comb(num_objs,2))
 #                                            #
 ##############################################
 
-if regional_option == 0:
+if regional_method == 'gaussian':
     samples = [obj0samples, obj1samples, obj2samples, obj3samples]
     mean = []
     std = []
@@ -135,48 +132,24 @@ if regional_option == 0:
         regional_weights[:,i] = t_link_cost(std[i],mean[i],actual_img).T
 
 else:
-    sample_mat = np.concatenate((obj0samples,obj1samples, obj2samples,obj3samples))    
-    sample_mat = sample_mat.reshape((sample_mat.size,1))
-    label_mat = np.array([0]*obj0samples.size + [1]*obj1samples.size + [2]*obj2samples.size + [3]*obj3samples.size)
- 
+    sample_mat = np.concatenate((obj0samples,obj1samples))    
+    #sample_mat = sample_mat.reshape((sample_mat.size,1))
+    label_mat = np.array([0]*samples0[0].shape[0] + [1]*samples1[0].shape[0])
+    
+    print sample_mat.shape
+    print label_mat.shape
     lgr = LogisticRegression()#solver='newton-cg')#,multi_class='multinomial')
     lgr.fit(sample_mat,label_mat)
+    regional_weights = np.abs(lgr.predict_log_proba()) 
 
-    t1 = time.time()
-    regional_weights = np.abs(lgr.predict_log_proba(actual_img)) 
 
-mins = np.argmin(regional_weights,axis=1)
-mins = mins.reshape((width,height))
-yellow_mask = mins == 0
-red_mask = mins == 1
-blue_mask = mins == 2
-green_mask = mins == 3
-
-seg_im = np.zeros((width,height,3))
-seg_im[yellow_mask] = [0,0,0]
-seg_im[red_mask] = [255,0,0]
-seg_im[blue_mask] = [0,0,255]
-seg_im[green_mask] = [0,255,0]
-print mins.shape
-print width*height
-imsave('mins.png',seg_im)
-imsave('class0.png',regional_weights[:,0].reshape((width,height)))
-imsave('class1.png',regional_weights[:,1].reshape((width,height)))
-imsave('class2.png',regional_weights[:,2].reshape((width,height)))
-imsave('class3.png',regional_weights[:,3].reshape((width,height)))
-for i in range(num_objs):
-   print "max regioinal weight for class " + str(i) + ' '+ str(np.max(regional_weights[:,i]))
-
-for i in range(num_objs):
-   print "min regioinal weight for class " + str(i) + ' '+str(np.min(regional_weights[:,i]))
-
-for i in range(num_objs):
-   print "mean regional weights weight for class " + str(i) +' ' + str(np.mean(regional_weights[:,i]))
 ##############################################
 #                                            #
 #  Set up boundary weights                   #
 #                                            #
 ##############################################
+
+brush_strokes = [obj0samples,obj1samples]
 
 # get adjacent edges
 # right edges = (left_node[i],right_node[i])
@@ -195,17 +168,22 @@ v1 = v1.reshape((v1.size))
 v2 = np.concatenate((right_nodes, down_nodes))
 v2 = v2.reshape((v2.size))
 
-if boundary_option == 0:
+if boundary_option == 'boykov':
     side_weights = neighbor_cost_boykov(actual_img[left_nodes],actual_img[right_nodes])
     vert_weights = neighbor_cost_boykov(actual_img[down_nodes],actual_img[up_nodes])
     boundary_weights = np.concatenate((side_weights,vert_weights))
     boundary_weights = boundary_weights.reshape((boundary_weights.size)).astype(np.float32) * cost_weight
-elif boundary_option == 1:
+
+elif boundary_option == 'gaussian':
+    means, stds = calculate_boundary_stats(brush_strokes,100)
+    combs = int(comb(num_objs,2))
+
     boundary_weights = np.zeros((v1.size,combs))
     diffs = np.abs(np.subtract(actual_img[v1],actual_img[v2]))
     for i in range(combs):
         boundary_weights[:,i] = t_link_cost(stds[i],means[i],diffs).T
     boundary_weights = boundary_weights.astype(np.float32) #* cost_weight
+
 else:
     lgr_bound = calculate_boundary_stats_lgr(brush_strokes,100)
     boundary_weights = np.abs(lgr_bound.predict_log_proba(np.abs(np.subtract(actual_img[v1],actual_img[v2])))).astype(np.float32) * cost_weight
@@ -214,9 +192,6 @@ else:
 boundary_weight_dict = {}
 for i in range(v1.shape[0]):
     boundary_weight_dict[(v1[i],v2[i])] = boundary_weights[i]
-
-filename = bound_dict[str(boundary_option)] + '_' + reg_dict[str(regional_option)] + '_stats'
-
 
 
 ###############################
@@ -273,10 +248,7 @@ while reps > 0:
         g.maxflow()
         
 
-        out = g.what_segment_vectorized()
-        print 'graph contains ' + str(graph_size)+ ' nodes'
-        print 'cut has ' + str(sum(out)) + ' ' +str(alpha) +' labels'
-        print 'cut has ' + str(graph_size - sum(out)) + ' ' + str(beta)+' labels' 
+        out = g.what_segment_vectorized() 
         # this needs speed up
         for i, label in enumerate(out):
             index = graph_indices[i]
@@ -293,9 +265,9 @@ green_mask = labels == 3
 np.savetxt('labels', labels)
 
 seg_im = np.zeros((width,height,3))
-seg_im[yellow_mask] = [0,0,0]
+seg_im[blue_mask] = [0,0,0]
 seg_im[red_mask] = [255,0,0]
-seg_im[blue_mask] = [0,0,255]
-seg_im[green_mask] = [0,255,0]
-imsave(output_file,seg_im)
-'''
+#seg_im[blue_mask] = [0,0,255]
+#seg_im[green_mask] = [0,255,0]
+save_loc = outdir + '_' + regional_method + '_' + boundary_method
+imsave(save_loc,seg_im)
